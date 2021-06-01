0eabe7b650f4744d7ac655bd0ce378d5ff99e80e
artj15 <artj15@gmail.com>
2021-06-01 11:30:48 +0300

    Initial commit

--- fabresh/fab_resh/views.py
+++ fabresh/fab_resh/views.py
@@ -11,20 +11,24 @@ class GetQuestion(GenericAPIView):
     permission_classes = (IsAuthenticated, )
     serializer_class = QuestionSerializer
     queryset = Questions.objects.all()
-    def get(self, request, format = None):
-        questions = Questions.objects.filter(visible = True)
-        last_point = QuestionSerializer(questions, many= True)
+
+    def get(self, request, format=None):
+        questions = Questions.objects.filter(visible=True)
+        last_point = QuestionSerializer(questions, many=True)
         return Response(last_point.data)
         # print(last_point.data)
         context = {
             'items': last_point.data,
         }
-        return  render('base.html', context)
+        return render('base.html', context)
+
+
 class QuestionsAnswers(GenericAPIView):
     permission_classes = (IsAuthenticated, )
     serializer_class = AnswerSerializer
-    def post(self, request, format = None):
-        answer = AnswerSerializer(data = request.data, context= request)
-        if answer.is_valid(raise_exception= True):
+
+    def post(self, request, format=None):
+        answer = AnswerSerializer(data=request.data, context=request)
+        if answer.is_valid(raise_exception=True):
             answer.save()
             return Response({'result': 'OK'})
